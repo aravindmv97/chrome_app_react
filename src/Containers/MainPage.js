e37c8c2a76796780032ae8d8e87e2b4ad6cb889d
aravind-manoharan97 <aravind@codingmart.com>
2018-11-28 20:07:50 +0530

    Background image and name

---
+++ src/Containers/MainPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './css/MainPage.css';
+
+class MainPage extends Component {
+
+    render() {
+        return (
+            <div>
+                <div className="mainContainer">
+                    <div className="content">
+                        <span>
+                            <b>Welcome, Aravind Manoharan</b>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
--- src/content.js
+++ src/content.js
@@ -5,14 +5,12 @@ import "./content.css";
 class Main extends React.Component {
     render() {
         return (
-            <div className={'my-extension'}>
-                <h1>Hello world - My first Extension</h1>
-            </div>
+            null
         )
     }
 }
 
 const app = document.createElement('div');
 app.id = "my-extension-root";
-document.body.appendChild(app);
+// document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
