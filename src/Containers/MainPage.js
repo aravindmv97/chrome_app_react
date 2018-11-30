0000121e8fb06652621206236c2ad1d924a4488b
aravind-manoharan97 <aravind@codingmart.com>
2018-11-30 13:36:00 +0530

    Background change on click and Flip Clock

--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+
+import Clock from '../Components/Clock';
+
+import Img1 from '../images/0.jpg';
+import Img2 from '../images/1.jpg';
+import Img3 from '../images/2.jpg';
+import Img4 from '../images/3.jpg';
+import Img5 from '../images/4.jpg';
+import Img6 from '../images/5.jpg';
+import Img7 from '../images/6.jpg';
+import Img8 from '../images/7.jpg';
+import Img9 from '../images/8.jpg';
+import Img10 from '../images/9.jpg';
+
 import './css/MainPage.css';
 
 class MainPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            images: [Img1, Img2, Img3, Img4],
+            current: 0,
+            bgImg: ''
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({ bgImg: this.state.images[0] })
+    }
+
+    changeBg = () => {
+        let state = this.state;
+        let index = state.current + 1;
+        if (index >= 4) {
+            index = 0
+        }
+        this.setState({ bgImg: state.images[index], current: index });
+        console.log(index)
+    }
+
 
     render() {
+
         return (
-            <div>
-                <div className="mainContainer">
+            <div onClick={this.changeBg}>
+                <div style={{ backgroundImage: `url(${this.state.bgImg})` }} className="mainContainer">
+                    <div className="clockContainer">
+                        <Clock />
+                    </div>
                     <div className="content">
-                        <span>
-                            <b>Welcome, Aravind Manoharan</b>
-                        </span>
+                        <span>Welcome, Aravind Manoharan</span>
                     </div>
                 </div>
             </div>
