4b99d6a6671216c4e939898f8cbb3a2a3291a943
aravindmv97 <aravindmv97@gmail.com>
2018-12-02 18:29:51 +0530

    Change background after every 5 mins

--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -8,10 +8,7 @@ import Img3 from '../images/2.jpg';
 import Img4 from '../images/3.jpg';
 import Img5 from '../images/4.jpg';
 import Img6 from '../images/5.jpg';
-import Img7 from '../images/6.jpg';
-import Img8 from '../images/7.jpg';
-import Img9 from '../images/8.jpg';
-import Img10 from '../images/9.jpg';
+
 
 import './css/MainPage.css';
 
@@ -19,7 +16,7 @@ class MainPage extends Component {
     constructor() {
         super();
         this.state = {
-            images: [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10],
+            images: [Img1, Img2, Img3, Img4, Img5, Img6],
             current: 0,
             bgImg: ''
         }
@@ -27,16 +24,18 @@ class MainPage extends Component {
 
     componentDidMount = () => {
         this.setState({ bgImg: this.state.images[0] })
+        setInterval(() => {
+            this.changeBg();
+        }, 50000)
     }
 
     changeBg = () => {
         let state = this.state;
         let index = state.current + 1;
-        if (index >= 4) {
+        if (index >= 6) {
             index = 0
         }
         this.setState({ bgImg: state.images[index], current: index });
-        console.log(index)
     }
 
 
